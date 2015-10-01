b97f3b0fecb4685c9563e81344a7c1aa92278d73
Stephen Sauceda <contact@stephensauceda.com>
2015-10-01 14:41:57 -0400

    spin up ghost server for tests #1

--- test/index.js
+++ test/index.js
@@ -1,8 +1,23 @@
-var ghost = require('../server.js');
+var ghost = require('ghost');
 var request = require('supertest');
 var config = require('../config');
+var path = require('path');
 
 describe('GET /', function(){
+  beforeEach(function(done) {
+    ghost({ config: path.join(__dirname, '/../config.js') }).then(function(ghostServer) {
+      ghostServer.start();
+      done()
+    });
+  });
+
+  afterEach(function(done) {
+    ghost().then(function(ghostServer) {
+      ghostServer.stop();
+      done();
+    });
+  });
+
   it('should return a 200', function(done){
     request(config.development.url)
       .get('/')
